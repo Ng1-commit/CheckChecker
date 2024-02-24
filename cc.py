5bccf1397b207347552c983e6956a4f31c72ffb9
Ng1-commit <patrick.goldsworthy@gmail.com>
2024-02-24 15:15:18 +0000

    gitignore update

--- cc.py
+++ cc.py
@@ -1,11 +1,10 @@
+from pickle import FALSE
 import random
 
 def generateRandomPieceSet():
     whitepieces = ['R', 'N', 'B', 'Q', 'B', 'N', 'R', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P']
-    blackpieces = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p']
-    
-    
-    #blackpieces = [item.lower() for item in whitepieces]
+    #blackpieces = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p']
+    blackpieces = [item.lower() for item in whitepieces]
 
     wP = ["K"]
     bP = ["k"]
@@ -65,20 +64,20 @@ def isWhiteKingInCheck(board):
 
     file = isKingInCheckOnFile(board, WKing, colour) 
     if file == True:
-    	print('Rank')
-    	return file
+        print('Rank')
+        return file
     rank = isKingInCheckOnRank(board, WKing, colour)
     if rank == True:
-    	print('File')
-    	return rank
+        print('File')
+        return rank
     diagonal = isKingInCheckOnDiagonal(board, WKing, colour) 
     if diagonal == True:
-    	print('Diagonal')
-    	return diagonal
+        print('Diagonal')
+        return diagonal
     knight = isKingInCheckFromKnight(board, WKing, colour)
     if knight == True:
-    	print('Knight')
-    	return knight
+        print('Knight')
+        return knight
     return isKingInCheck
 
 def isKingInCheckOnFile(board, kingPos, colour):
@@ -131,7 +130,7 @@ def isKingInCheckOnRank(board, kingPos, colour):
             else:
                 return False
 
-def isKingInCheckOnDiagonal(board, kingPos, colour): # to fix on a keyboard
+def isKingInCheckOnDiagonal(board, kingPos, colour):
  
     currentKingPos = list(kingPos)
     if colour == 'w':
@@ -142,7 +141,7 @@ def isKingInCheckOnDiagonal(board, kingPos, colour): # to fix on a keyboard
             if board[(currentKingPos[0], currentKingPos[1])] == '': 
                 continue
             elif board[(currentKingPos[0],  currentKingPos[1])] in ['q', 'b']:
-                print(chr(currentKingPos[0] +96) + ',' + str(currentKingPos[1])  + 'below and left')
+                print(chr(currentKingPos[0] +96) + ',' + str(currentKingPos[1])  + ' below and left')
                 return True
             else:
                 return False
@@ -155,7 +154,7 @@ def isKingInCheckOnDiagonal(board, kingPos, colour): # to fix on a keyboard
             if board[(currentKingPos[0], currentKingPos[1])] == '': 
                 continue
             elif board[(currentKingPos[0],  currentKingPos[1])] in ['q', 'b']:
-                print(chr(currentKingPos[0] +96) + ',' + str(currentKingPos[1])  + 'below and right')
+                print(chr(currentKingPos[0] +96) + ',' + str(currentKingPos[1])  + ' below and right')
                 return True
             else:
                 return False
@@ -171,7 +170,7 @@ def isKingInCheckOnDiagonal(board, kingPos, colour): # to fix on a keyboard
                 continue
             elif board[(currentKingPos[0],  currentKingPos[1])] in possiblePieces:
                 print(possiblePieces)
-                print(chr(currentKingPos[0] +96) + ',' + str(currentKingPos[1])  + 'above and right')
+                print(chr(currentKingPos[0] +96) + ',' + str(currentKingPos[1])  + ' above and right')
                 return True
             else:
                 return False
@@ -193,18 +192,16 @@ def isKingInCheckOnDiagonal(board, kingPos, colour): # to fix on a keyboard
                 return False
 
 def doesKnightAttackSquare(kingPos):
-	 
-	 x = kingPos[0]
-	 y = kingPos[1]
-	 
-	 attackedSquares = [ 
+    x = kingPos[0]
+    y = kingPos[1]
+    attackedSquares = [ 
 	 (x-1, y-2), (x-1, y+2),
 	 (x-2, y-1),(x-2, y+1),
 	 (x+1, y-2), (x+1, y+2), 
 	 (x+2, y+1), (x+2, y-1)
 	 ]
 	 
-	 return attackedSquares
+    return attackedSquares
 
 def isKingInCheckFromKnight(board, kingPos, colour): 
 	if colour == 'w':
@@ -229,13 +226,13 @@ def printBoard(board):
 def test():
     pieces = generateRandomPieceSet()
     board = generateboard()
-    #board = {(1, 1): '', (1, 2): 'p', (1, 3): '', (1, 4): '', (1, 5): 'p', (1, 6): '', (1, 7): 'p', (1, 8): '', (2, 1): '', (2, 2): '', (2, 3): '', (2, 4): '', (2, 5): '', (2, 6): 'p', (2, 7): 'p', (2, 8): '', (3, 1): '', (3, 2): 'K', (3, 3): 'p', (3, 4): '', (3, 5): '', (3, 6): '', (3, 7): 'p', (3, 8): '', (4, 1): '', (4, 2): '', (4, 3): '', (4, 4): '', (4, 5): '', (4, 6): '', (4, 7): '', (4, 8): '', (5, 1): '', (5, 2): 'p', (5, 3): '', (5, 4): '', (5, 5): '', (5, 6): '', (5, 7): '', (5, 8): '', (6, 1): '', (6, 2): '', (6, 3): '', (6, 4): '', (6, 5): '', (6, 6): 'p', (6, 7): '', (6, 8): '', (7, 1): '', (7, 2): '', (7, 3): '', (7, 4): '', (7, 5): '', (7, 6): '', (7, 7): '', (7, 8): '', (8, 1): '', (8, 2): '', (8, 3): 'k', (8, 4): '', (8, 5): '', (8, 6): 'p', (8, 7): 'p', (8, 8): ''}
     placepieces(board, pieces)
     print(str(board))
     printBoard(board)
     return isWhiteKingInCheck(board)
-	
-isInCheck = False
+
+
+isInCheck = FALSE
 while isInCheck == False:
-	isInCheck = test()
-	print('\n')
+    isInCheck = test()
+    print('\n')
